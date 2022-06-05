155bf31e770e170377eb3dd453ff7fffce16404b
Haris131 <41672454+Haris131@users.noreply.github.com>
2022-06-05 19:58:36 +0700

    Add files via upload

---
+++ time_sync.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Automatically Updates System Time According to the NIST Atomic Clock in a Linux Environment
+while true; do
+nistTime=$(curl -10 -s -I --insecure 'https://my.xl.co.id' | grep "ate:")
+echo $nistTime
+dateString=$(echo $nistTime | cut -d' ' -f2-7)
+dayString=$(echo $nistTime | cut -d' ' -f2-2)
+dateValue=$(echo $nistTime | cut -d' ' -f3-3)
+monthValue=$(echo $nistTime | cut -d' ' -f4-4)
+yearValue=$(echo $nistTime | cut -d' ' -f5-5)
+timeValue=$(echo $nistTime | cut -d' ' -f6-6)
+timeZoneValue=$(echo $nistTime | cut -d' ' -f7-7)
+case $monthValue in
+  "Jan")
+  monthValue="01"
+  ;;
+  "Feb")
+  monthValue="02"
+  ;;
+  "Mar")
+  monthValue="03"
+  ;;
+  "Apr")
+  monthValue="04"
+  ;;
+  "May")
+  monthValue="05"
+  ;;
+  "Jun")
+  monthValue="06"
+  ;;
+  "Jul")
+  monthValue="07"
+  ;;
+  "Aug")
+  monthValue="08"
+  ;;
+ "Sep")
+  monthValue="09"
+  ;;
+  "Oct")
+  monthValue="10"
+  ;;
+  "Nov")
+  monthValue="11"
+  ;;
+  "Dec")
+  monthValue="12"
+  ;;
+  *)
+  continue
+esac
+if [ "$dateString" ]; then
+  date --utc --set $yearValue.$monthValue.$dateValue-$timeValue
+  date
+  exit
+fi
+done
